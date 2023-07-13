8279a89cc859a524983ce2b882783640a9c8d66c
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2023-07-13 22:35:11 +0100

    .

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -24,11 +24,11 @@ export default async function PostPage({ params }: Props) {
   }
 
   return (
-    <div className="bg-zinc-50 min-h-screen bg-gradient-to-tl from-black via-zinc-900 to-black">
+    <div className="bg-zinc-50 min-h-screen bg-gradient-to-tl from-black via-zinc-900 to-black pb-10">
       <Header project={project} />
 
       {project?.image && (
-        <div className="flex flex-col items-center justify-center w-screen overflow-hidden ">
+        <div className="flex flex-col items-center justify-center w-screen overflow-hidden">
           <Image
             src={project?.image}
             width={500}
@@ -37,6 +37,20 @@ export default async function PostPage({ params }: Props) {
           />
         </div>
       )}
+
+      <div className="mx-auto my-10 py-10 max-w-6xl w-6xl px-6 lg:px-8 flex flex-col items-center">
+        {project?.info && (
+          <div>
+            <h2 className="text-2xl text-zinc-500 mb-2 pb-2 font-bold">
+              About this project
+            </h2>
+
+            <hr className="mb-8" />
+
+            {project?.info}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
